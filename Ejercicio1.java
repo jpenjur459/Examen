b9caf8a5e00ede01a896ed4bde5e9ce386670d12
jpenjur459 <jpenjur459@g.educaand.es>
2023-10-04 09:53:31 +0200

    Cambio

---
+++ Ejercicio1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("inserte el valor de a ");
+        int valor_a = entrada.nextInt();
+
+        System.out.println("inserte el valor de b ");
+        int valor_b = entrada.nextInt();
+
+        System.out.println("inserte el valor de c ");
+        int valor_c = entrada.nextInt();
+
+        int resultado1 = valor_a * valor_b + valor_c * ((3-valor_a)/2*valor_b);
+        System.out.println("el valor de la operación a es" resultado1);
+
+        int resultado2 = ((2 + valor_a * valor_b)/4 )^(valor_c + 2);
+        System.out.println("el valor de la operación b es" resultado2);
+        int resultado
+
+    }
+}
--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,4 +1,4 @@
-public class Examen {
+public class Ejercicio3 {
     public static void main(String[] args) {
 
     }
